f247f2bff592fc0169974f920b4358f7dc82440b
Scott Weinstein <sweinstein27@gmail.com>
2018-12-02 17:34:19 +0000

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,23 @@
 // Code your JavaScript / jQuery solution here
+
+const WINNING_COMBOS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6],
+                        [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
+
+var player = () => turn % 2 ?  'O' : 'X';
+var turn = 0;
+var currentGame = 0;
+
+$(document).ready(function (){
+  $("#save").click(function() {
+  alert("saved me!");
+  });
+});
+
+function updateState(square) {
+  var token = player()
+  $(square).text(token);
+}
+
+function setMessage(string){
+  $('#message').text(string);
+}
